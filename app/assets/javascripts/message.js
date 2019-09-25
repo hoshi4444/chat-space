03c1d7b219b675e420116bd6f1a784ed5299881f
hoshi4444 <hoshi44444444@gmail.com>
2019-09-25 16:45:24 +0900

    edit-auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,52 @@
 $(function(){
-  function buildHTML(message){
-    
-  var img = message.image !== null ? `<img src=${message.image} class: message__text__image >` : ""
 
-  var html = `<div class="message">
-                <div class="message__upper-info">
-                  <div class="message__upper-info__talker">
-                   ${message.user_name}
-                  </div>
-                  <div class="message__upper-info__date">
-                    ${message.created_at}
-                  </div>
-                </div>
-                <div class="message__text">
-                  ${message.content}
-                  ${img}
-                </div>
-              </div>`
-  return html;
-  }
+  var buildMessageHTML = function(message) {
+   if (message.content && message.image) {
+     var html = '<div class="message" data-message_id=' + message.id + '>' +
+                  '<div class="message__upper-info">' +
+                    '<div class="message__upper-info__talker">' +
+                      message.user_name +
+                    '</div>' +
+                    '<div class="message__upper-info__date">' +
+                      message.created_at +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="message__text">' +
+                      message.content +
+                    '<img src="' + message.image + '" class="message__text__image" >' +
+                  '</div>' +
+                '</div>'
+   } else if (message.content) {
+     var html = '<div class="message" data-message_id=' + message.id + '>' +
+                  '<div class="message__upper-info">' +
+                    '<div class="message__upper-info__talker">' +
+                      message.user_name +
+                    '</div>' +
+                    '<div class="message__upper-info__date">' +
+                      message.created_at +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="message__text">' +
+                      message.content +
+                  '</div>' +
+                '</div>'
+   } else if (message.image) {
+     var html = '<div class="message" data-message_id=' + message.id + '>' +
+                  '<div class="message__upper-info">' +
+                    '<div class="message__upper-info__talker">' +
+                      message.user_name +
+                    '</div>' +
+                    '<div class="message__upper-info__date">' +
+                      message.created_at +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="message__text">' +
+                    '<img src="' + message.image + '" class="message__text__image" >' +
+                  '</div>' +
+                '</div>'
+   };
+   return html;
+ };
     $('#new_message').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
@@ -32,7 +60,7 @@ $(function(){
         contentType: false
       })
     .done(function(data){
-      var html = buildHTML(data)
+      var html = buildMessageHTML(data)
       $('.messages').append(html).animate({scrollTop:$('.messages')[0].scrollHeight},'fast');
       $('form')[0].reset();
     })
@@ -43,4 +71,27 @@ $(function(){
       $('input').removeAttr("disabled");
       });
   })
+   
+  if(document.URL.match("/messages")) {
+   var reloadMessages = function() {
+     last_message_id = $('.message:last').data('message_id')
+     $.ajax({
+       url: 'api/messages',
+       type: 'get',
+       dataType: 'json',
+       data: {id: last_message_id}
+     })
+     .done(function(messages) {
+       var insertHTML = '';
+       messages.forEach(function(message){
+       insertHTML += buildMessageHTML(message)
+       })
+       $('.messages').append(insertHTML).animate({scrollTop:$('.messages')[0].scrollHeight},'fast');
+     })
+     .fail(function() {
+       alert('通信に失敗しました。')
+     });
+   };
+   setInterval(reloadMessages, 5000)
+  }
 });
