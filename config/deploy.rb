25a98ee3df70d397a58f618cfa3d6554d780e2f8
hoshi4444 <hoshi44444444@gmail.com>
2019-09-27 22:14:00 +0900

    edit-dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,12 @@
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.1"
+lock '3.11.1'
 
 set :application, 'chat-space'
 set :repo_url, 'git@github.com:hoshi4444/chat-space.git'
@@ -26,6 +33,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 set :linked_files, %w{ config/secrets.yml }
+
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -66,9 +74,3 @@ end
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
