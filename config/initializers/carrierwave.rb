401361a14442fd18a75db52f62191c13f2deb6d1
hoshi4444 <hoshi44444444@gmail.com>
2019-09-27 14:38:45 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' 
   }
 
-  config.fog_directory  = 'hoshi4444'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hoshi4444'
+  config.fog_directory  = 'hoshichat'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hoshichat'
 end
